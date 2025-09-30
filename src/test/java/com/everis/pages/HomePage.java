a22b36b7631f94dc9a631e061dce7e83de969da4
Pedro Mota <80173371+pmotadeee@users.noreply.github.com>
2025-09-30 16:09:23 -0300

    Update HomePage.java

--- src/test/java/com/everis/pages/HomePage.java
+++ src/test/java/com/everis/pages/HomePage.java
@@ -6,22 +6,38 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.everis.util.Hooks;
 
+/**
+ * HomePage represents the application's main page.
+ * It provides interactions with the search bar to look up products.
+ * 
+ * Inherits from BasePage to reuse logging and Selenium helper methods.
+ */
 public class HomePage extends BasePage {
 
-	@FindBy(css = "#search_query_top")
-	protected WebElement campoBusca;
-	
-	@FindBy(name = "submit_search")
-	protected WebElement botaoLupaBuscar;
-	
-	public HomePage() {
-		PageFactory.initElements(Hooks.getDriver(), this);
-	}
+    /** Search input field on the home page */
+    @FindBy(css = "#search_query_top")
+    protected WebElement searchField;
 
-	public void pesquisarProduto(String nomeProduto) {
-		campoBusca.sendKeys(nomeProduto);
-		botaoLupaBuscar.click();
-		log("Pesquisou pelo produto: " + nomeProduto);
-	}
+    /** Search button (magnifying glass icon) */
+    @FindBy(name = "submit_search")
+    protected WebElement searchButton;
 
+    /**
+     * Constructor initializes the web elements on this page
+     * using Selenium's PageFactory.
+     */
+    public HomePage() {
+        PageFactory.initElements(Hooks.getDriver(), this);
+    }
+
+    /**
+     * Searches for a product using the search bar and search button.
+     *
+     * @param productName the product name to be searched
+     */
+    public void searchProduct(String productName) {
+        searchField.sendKeys(productName);
+        searchButton.click();
+        log("Searched for the product: " + productName);
+    }
 }

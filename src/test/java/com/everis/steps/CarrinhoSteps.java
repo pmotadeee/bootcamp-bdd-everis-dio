c9530b1ff08405c2d4aa68c5f37ea2a08a01187a
Pedro Mota <80173371+pmotadeee@users.noreply.github.com>
2025-09-30 16:13:42 -0300

    Update CarrinhoSteps.java

--- src/test/java/com/everis/steps/CarrinhoSteps.java
+++ src/test/java/com/everis/steps/CarrinhoSteps.java
@@ -6,13 +6,23 @@ import com.everis.pages.CarrinhoPage;
 
 import io.cucumber.java.pt.Entao;
 
+/**
+ * CarrinhoSteps defines the Cucumber step definitions related to the cart.
+ * It validates whether the expected product is present in the shopping cart.
+ */
 public class CarrinhoSteps {
-	
-	@Entao("^o produto \"(.*)\" deve estar presente no carrinho$")
-	public void apresentouProdutoEsperadoNoCarrinho(String nomeProduto) {
-		CarrinhoPage carrinhoPage = new CarrinhoPage();
-		Assert.assertTrue("Deveria ter apresentado o produto [" + nomeProduto + "] no carrinho", 
-				carrinhoPage.apresentouProdutoEsperadoNoCarrinho(nomeProduto));
-	}
-	
+
+    /**
+     * Step definition that asserts a product is present in the cart.
+     * 
+     * @param productName the name of the product that should be displayed in the cart
+     */
+    @Entao("^o produto \"(.*)\" deve estar presente no carrinho$")
+    public void shouldDisplayExpectedProductInCart(String productName) {
+        CarrinhoPage carrinhoPage = new CarrinhoPage();
+        Assert.assertTrue(
+                "The product [" + productName + "] should have been displayed in the cart.",
+                carrinhoPage.apresentouProdutoEsperadoNoCarrinho(productName)
+        );
+    }
 }

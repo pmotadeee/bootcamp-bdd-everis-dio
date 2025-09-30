c2a03a1dc9eaac47e633e53ad85a724ffbc12102
Pedro Mota <80173371+pmotadeee@users.noreply.github.com>
2025-09-30 16:23:14 -0300

    Update Hooks.java

--- src/test/java/com/everis/util/Hooks.java
+++ src/test/java/com/everis/util/Hooks.java
@@ -29,135 +29,175 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+/**
+ * Hooks class integrates with JUnit and Cucumber lifecycle.
+ * - Initializes reporting structure (ExtentReports).
+ * - Manages WebDriver lifecycle (setup, quit).
+ * - Handles before/after scenario actions including logging and screenshots.
+ */
 public class Hooks extends TestWatcher {
 
-	private static WebDriver driver;
-	private static ExtentReports extentReport;
-	private static Scenario scenario;
-	private static ExtentTest extentTest;
+    private static WebDriver driver;
+    private static ExtentReports extentReport;
+    private static Scenario scenario;
+    private static ExtentTest extentTest;
 
-	public Hooks() {
-		super();
-	}
+    public Hooks() {
+        super();
+    }
 
-	@Override
-	protected void starting(Description description) {
-		super.starting(description);
-		new File("target/report").mkdir();
-		new File("target/report/html").mkdir();
-		new File("target/report/html/img").mkdir();
-		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("target/report/html/" + description.getDisplayName().replace("tests.", "") + ".html");
-		htmlReporter.config().setEncoding("ISO-8859-1");
-		extentReport = new ExtentReports();
-		extentReport.attachReporter(htmlReporter);
-		extentReport.setSystemInfo("os.name", System.getProperty("os.name"));
-	}
+    /**
+     * Executed before test execution starts.
+     * Initializes reporting directories and ExtentReports.
+     *
+     * @param description JUnit test description
+     */
+    @Override
+    protected void starting(Description description) {
+        super.starting(description);
 
-	@Before
-	public void beforeCenario(Scenario scenario) {
-	    Hooks.scenario = scenario;
-		extentTest = extentReport.createTest("Cenario: " + scenario.getName(), scenario.getName());
-		extentTest.assignCategory("feature:" + scenario.getId().replaceAll(";.*", ""));
-		Collection<String> tags = scenario.getSourceTagNames();
-		for (String tag : tags) {
-			extentTest.assignCategory(tag);
-		}
-		System.out.println("Cenario: " + scenario.getName());
-	}
-	
-	@After
-	public void afterCenario() throws IOException{
-    	if(scenario.isFailed()){
-    		if (driver != null) {
-        		BasePage basePage = new BasePage();
-        		basePage.logPrintFail("The test is failed");
-        		
-        		Throwable throwable = logError(scenario);
-        		extentTest.fail(throwable);
-			}
-    	}
-    	extentReport.flush();
-    	if (driver != null) {
-    		driver.quit();
-		}
-	}
-	
-	private Throwable logError(Scenario scenario) {
-		Field field = FieldUtils.getField(Scenario.class, "delegate", true);
-		Method getError = null;
-	    try {
-	        final TestCaseState testCase = (TestCaseState) field.get(scenario);
-	        if (getError == null) {
-	            getError = MethodUtils.getMatchingMethod(testCase.getClass(), "getError");
-	            getError.setAccessible(true);
-	        }
-	        return (Throwable) getError.invoke(testCase);
-	    } catch (Exception e) {
-	    	System.err.println("error receiving exception" + e);
-	    }
-	    return null;
-	}
-	
-	@Override
-	protected void finished(Description description) {
-		super.finished(description);
-	}
+        new File("target/report/html/img").mkdirs();
 
-	public static WebDriver getDriver() {
-		return driver;
-	}
-	
-	public static ExtentTest getExtentTest() {
-		return extentTest;
-	}
-	
-	public static Scenario getScenario() {
-		return scenario;
-	}
-	
-	public static ExtentReports getExtentReports(){
-		return extentReport;
-	}
-	
-	public static void navigateToULRChrome(String url){
-		String downloadFilepath = System.getProperty("user.dir") + "/target/temp";
-		
-		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("download.default_directory", downloadFilepath);
-		chromePrefs.put("credentials_enable_service", false);
-		
-		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("prefs", chromePrefs);
-		options.addArguments("disable-infobars");
-		options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.ACCEPT);
-		
-		WebDriverManager.chromedriver().setup();
-		
-		driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.navigate().to(url);
-	}
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(
+                "target/report/html/" + description.getDisplayName().replace("tests.", "") + ".html"
+        );
+        htmlReporter.config().setEncoding("ISO-8859-1");
 
-	
-	public static void navigateToULRChromeMobile(String url){
-		String downloadFilepath = System.getProperty("user.dir") + "/target/temp";
-		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("download.default_directory", downloadFilepath);
-		chromePrefs.put("credentials_enable_service", false);
-		
-		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("prefs", chromePrefs);
-		options.addArguments("disable-infobars");
-		options.addArguments("--disable-print-preview");
-			
-		Map<String, String> mobileEmulation = new HashMap<String, String>();
-		mobileEmulation.put("deviceName", "iPhone X");
-		options.setExperimentalOption("mobileEmulation", mobileEmulation);
+        extentReport = new ExtentReports();
+        extentReport.attachReporter(htmlReporter);
+        extentReport.setSystemInfo("os.name", System.getProperty("os.name"));
+    }
 
-		WebDriverManager.chromedriver().setup();
-		
-		driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.navigate().to(url);
-	}
+    /**
+     * Runs before each Cucumber scenario.
+     * Creates a new ExtentTest instance and assigns scenario metadata.
+     *
+     * @param scenario the current Cucumber scenario
+     */
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        Hooks.scenario = scenario;
+        extentTest = extentReport.createTest("Scenario: " + scenario.getName(), scenario.getName());
+        extentTest.assignCategory("feature:" + scenario.getId().replaceAll(";.*", ""));
+
+        Collection<String> tags = scenario.getSourceTagNames();
+        for (String tag : tags) {
+            extentTest.assignCategory(tag);
+        }
+
+        System.out.println("Scenario: " + scenario.getName());
+    }
+
+    /**
+     * Runs after each Cucumber scenario.
+     * Logs failures with screenshots and flushes the ExtentReport.
+     *
+     * @throws IOException if screenshot saving fails
+     */
+    @After
+    public void afterScenario() throws IOException {
+        if (scenario.isFailed() && driver != null) {
+            BasePage basePage = new BasePage();
+            basePage.logPrintFail("The test has failed.");
+
+            Throwable throwable = logError(scenario);
+            extentTest.fail(throwable);
+        }
+
+        extentReport.flush();
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    /**
+     * Extracts error/exception information from a failed scenario
+     * using reflection (Cucumber internal API).
+     *
+     * @param scenario the failed scenario
+     * @return Throwable error if found, otherwise null
+     */
+    private Throwable logError(Scenario scenario) {
+        try {
+            Field field = FieldUtils.getField(Scenario.class, "delegate", true);
+            final TestCaseState testCase = (TestCaseState) field.get(scenario);
+
+            Method getError = MethodUtils.getMatchingMethod(testCase.getClass(), "getError");
+            getError.setAccessible(true);
+
+            return (Throwable) getError.invoke(testCase);
+        } catch (Exception e) {
+            System.err.println("Error retrieving exception: " + e);
+            return null;
+        }
+    }
+
+    @Override
+    protected void finished(Description description) {
+        super.finished(description);
+    }
+
+    // ===== Getters =====
+
+    public static WebDriver getDriver() { return driver; }
+
+    public static ExtentTest getExtentTest() { return extentTest; }
+
+    public static Scenario getScenario() { return scenario; }
+
+    public static ExtentReports getExtentReports() { return extentReport; }
+
+    // ===== Browser Navigation =====
+
+    /**
+     * Opens Chrome browser with predefined options and navigates to the given URL.
+     *
+     * @param url the target website URL
+     */
+    public static void navigateToULRChrome(String url) {
+        String downloadFilepath = System.getProperty("user.dir") + "/target/temp";
+
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        chromePrefs.put("credentials_enable_service", false);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("disable-infobars");
+        options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.ACCEPT);
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.navigate().to(url);
+    }
+
+    /**
+     * Opens Chrome browser in mobile emulation mode and navigates to the given URL.
+     *
+     * @param url the target website URL
+     */
+    public static void navigateToULRChromeMobile(String url) {
+        String downloadFilepath = System.getProperty("user.dir") + "/target/temp";
+
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        chromePrefs.put("credentials_enable_service", false);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("disable-infobars", "--disable-print-preview");
+
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
+        options.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.navigate().to(url);
+    }
 }

c7c519cf5c8a6d1ade8426a875ffd265330e14ad
Pedro Mota <80173371+pmotadeee@users.noreply.github.com>
2025-09-30 16:16:52 -0300

    Update ResultadoPesquisaSteps.java

--- src/test/java/com/everis/steps/ResultadoPesquisaSteps.java
+++ src/test/java/com/everis/steps/ResultadoPesquisaSteps.java
@@ -4,12 +4,22 @@ import com.everis.pages.ResultadoPesquisaPage;
 
 import io.cucumber.java.pt.Quando;
 
+/**
+ * ResultadoPesquisaSteps defines the Cucumber step definitions
+ * for actions performed on the search results page.
+ * It covers adding a product from the results list to the shopping cart.
+ */
 public class ResultadoPesquisaSteps {
 
-	@Quando("^adiciona o produto \"(.*)\" ao carrinho$")
-	public void adicionarProdutoAoCarrinho(String nomeProduto) {
-		ResultadoPesquisaPage resultadoPesquisaPage = new ResultadoPesquisaPage();
-		resultadoPesquisaPage.adicionarProdutoAoCarrinho(nomeProduto);
-	}
-	
+    /**
+     * Step definition that adds a product to the shopping cart
+     * from the search results page.
+     *
+     * @param productName the name of the product to be added to the cart
+     */
+    @Quando("^adiciona o produto \"(.*)\" ao carrinho$")
+    public void addProductToCart(String productName) {
+        ResultadoPesquisaPage resultadoPesquisaPage = new ResultadoPesquisaPage();
+        resultadoPesquisaPage.addProductToCart(productName);
+    }
 }

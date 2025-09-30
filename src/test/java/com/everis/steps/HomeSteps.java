c537efc0f5e32e9bbd8282b05fbc3fe3d3615766
Pedro Mota <80173371+pmotadeee@users.noreply.github.com>
2025-09-30 16:15:32 -0300

    Update HomeSteps.java

--- src/test/java/com/everis/steps/HomeSteps.java
+++ src/test/java/com/everis/steps/HomeSteps.java
@@ -6,17 +6,31 @@ import com.everis.util.Hooks;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 
+/**
+ * HomeSteps defines the Cucumber step definitions for the home page.
+ * It handles actions such as navigating to the site and searching for products.
+ */
 public class HomeSteps {
 
-	@Dado("^que um usuario acessa o site \"(.*)\"$")
-	public void acessarSite(String url) {
-		Hooks.navigateToULRChrome(url);
-	}
-	
-	@E("^pesquisa pelo produto \"(.*)\"$")
-	public void pesquisarProduto(String nomeProduto) {
-		HomePage homePage = new HomePage();
-		homePage.pesquisarProduto(nomeProduto);
-	}
-	
+    /**
+     * Step definition that opens the given site URL in Chrome.
+     *
+     * @param url the website URL to be accessed
+     */
+    @Dado("^que um usuario acessa o site \"(.*)\"$")
+    public void accessSite(String url) {
+        Hooks.navigateToULRChrome(url);
+    }
+
+    /**
+     * Step definition that searches for a product by its name
+     * using the search bar on the home page.
+     *
+     * @param productName the name of the product to search for
+     */
+    @E("^pesquisa pelo produto \"(.*)\"$")
+    public void searchProduct(String productName) {
+        HomePage homePage = new HomePage();
+        homePage.searchProduct(productName);
+    }
 }
